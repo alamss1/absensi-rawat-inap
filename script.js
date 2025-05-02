73c06cb489ce9e1eafcb2a33b29f0b2b5a394d3b
alamss1 <alam.puskesmaspagak@gmail.com>
2025-05-02 09:28:38 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.getElementById("absenForm").addEventListener("submit", async (e) => {
+document.getElementById("absenForm").addEventListener("submit", async function (e) {
   e.preventDefault();
   const form = e.target;
   const data = {
@@ -7,12 +7,18 @@ document.getElementById("absenForm").addEventListener("submit", async (e) => {
     waktu: new Date().toISOString()
   };
 
-  const res = await fetch("https://script.google.com/macros/s/AKfycbwV8MeKtCRaAhqVMFlHwKprv7gkw0aa1aY9ujjydDTNv4yC0TW1yMBjn6QKctShoeGU/exec", {
-    method: "POST",
-    headers: {"Content-Type": "application/json"},
-    body: JSON.stringify(data)
-  });
+  try {
+    const res = await fetch("https://script.google.com/macros/s/AKfycbzH8O3OsVxT-Xz_NI38tXaUlair2a3ydNezC38Z2UWyWLkSZixPrfrFDv0mGB_evWEa/exec", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data)
+    });
 
-  const hasil = await res.text();
-  document.getElementById("respon").textContent = hasil;
+    const hasil = await res.text();
+    document.getElementById("respon").textContent = hasil;
+    form.reset();
+  } catch (error) {
+    document.getElementById("respon").textContent = "Gagal mengirim. Coba lagi.";
+    console.error(error);
+  }
 });
